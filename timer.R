7e6a6027a3525199a2ee1aff02dcdc18e63ffb8c
Victoria Olsen Tvedt <Victoria@Victorias-MacBook-Pro.local>
2023-10-19 16:59:42 +0200

    Created 4 new scripts, 3 for executing/sovling the problems, and 1 timer script

---
+++ timer.R
@@ -0,0 +1,24 @@
+library(tictoc)
+library(tibble)
+
+
+# Measure time elapsed for the solution as it is originally
+tic()
+source(file.path("scripts", "script_as_is.R"))
+time_elapsed_as_is <- toc()
+
+# Measure time elapsed for parallel computing
+tic()
+source(file.path("scripts", "script_parallel_computing.R"))
+time_elapsed_parallel <- toc()
+
+# Create a tibble with the time elapsed results
+time_elapsed_tibble <- tibble(
+  Script = c("script_as_is", "script_parallel_computing"),
+  TimeElapsed = c(time_elapsed_as_is$toc - time_elapsed_as_is$tic,
+                  time_elapsed_parallel$toc - time_elapsed_parallel$tic)
+)
+
+# Print the tibble
+print(time_elapsed_tibble)
+
